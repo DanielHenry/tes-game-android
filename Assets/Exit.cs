e6edbf9ea7fbadd7c37d30fdacab07c7e30fd8ba
Daniel Henry <daniel.henry.39@gmail.com>
2018-02-19 06:27:13 +0700

    First Blood

---
+++ Assets/Exit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void buttonYa()
+	{
+		Application.Quit();
+	}
+	public void buttonTidak()
+	{
+		SceneManager.LoadScene("utama");
+	}
+}
---
+++ Assets/utama.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class utama : MonoBehaviour {
+
+	private GameObject after_button_text;
+
+	// Use this for initialization
+	void Start () {
+		after_button_text = this.gameObject.transform.GetChild(1).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			SceneManager.LoadScene("exit_confirmation");
+		}
+	}
+
+	public void tombolSatu()
+	{
+		after_button_text.GetComponent<Text>().text = "satu";
+	}
+	public void tombolDua()
+	{
+		after_button_text.GetComponent<Text>().text = "dua";
+	}
+	public void tombolTiga()
+	{
+		after_button_text.GetComponent<Text>().text = "tiga";
+	}
+	public void tombolEmpat()
+	{
+		after_button_text.GetComponent<Text>().text = "empat";
+	}
+}
